1be35b5fc6dba43d68b31a2d2b6eb6d0bd2842be
Rami Yassein Eltayeb Mohamed <149052853+Ramimoha1@users.noreply.github.com>
2025-06-19 20:57:39 +0800

    Update data_insert.sql

--- sql/data_insert.sql
+++ sql/data_insert.sql
@@ -22,59 +22,60 @@ INSERT INTO Trait (name) VALUES
 
 -- INSERT MajorTraits !NOT READY YET DON'T ADD UNTILL WE DETERMINE THE WEIGHTS
 
--- Computer Science
+-- 1. Computer Science (9 + 5 + 1 + 4 + 1 = 20)
 INSERT INTO MajorTrait (major_id, trait_id, weight) VALUES
 (1, 1, 9),  -- Analytical
-(1, 2, 6),  -- Creative
-(1, 3, 2),  -- Social
-(1, 4, 8),  -- Organized
-(1, 5, 4);  -- Hands-on
+(1, 2, 5),  -- Creative
+(1, 3, 1),  -- Social
+(1, 4, 4),  -- Organized
+(1, 5, 1);  -- Hands-on
 
--- Psychology
+-- 2. Psychology (4 + 2 + 8 + 5 + 1 = 20)
 INSERT INTO MajorTrait (major_id, trait_id, weight) VALUES
-(2, 1, 7),  -- Analytical
-(2, 2, 5),  -- Creative
-(2, 3, 9),  -- Social
-(2, 4, 6),  -- Organized
-(2, 5, 3);  -- Hands-on
+(2, 1, 4),  -- Analytical
+(2, 2, 2),  -- Creative
+(2, 3, 8),  -- Social
+(2, 4, 5),  -- Organized
+(2, 5, 1);  -- Hands-on
 
--- Engineering
+-- 3. Engineering (8 + 1 + 1 + 5 + 5 = 20)
 INSERT INTO MajorTrait (major_id, trait_id, weight) VALUES
-(3, 1, 9),  -- Analytical
-(3, 2, 5),  -- Creative
-(3, 3, 3),  -- Social
-(3, 4, 8),  -- Organized
-(3, 5, 8);  -- Hands-on
+(3, 1, 8),  -- Analytical
+(3, 2, 1),  -- Creative
+(3, 3, 1),  -- Social
+(3, 4, 5),  -- Organized
+(3, 5, 5);  -- Hands-on
 
--- Business
+-- 4. Business (6 + 4 + 6 + 3 + 1 = 20)
 INSERT INTO MajorTrait (major_id, trait_id, weight) VALUES
-(4, 1, 8),  -- Analytical
-(4, 2, 7),  -- Creative
-(4, 3, 8),  -- Social
-(4, 4, 7),  -- Organized
-(4, 5, 5);  -- Hands-on
+(4, 1, 6),  -- Analytical
+(4, 2, 4),  -- Creative
+(4, 3, 6),  -- Social
+(4, 4, 3),  -- Organized
+(4, 5, 1);  -- Hands-on
 
--- Biomedical Sciences
+-- 5. Biomedical Sciences (7 + 1 + 2 + 6 + 4 = 20)
 INSERT INTO MajorTrait (major_id, trait_id, weight) VALUES
-(5, 1, 9),  -- Analytical
-(5, 2, 5),  -- Creative
-(5, 3, 4),  -- Social
-(5, 4, 8),  -- Organized
-(5, 5, 6);  -- Hands-on
+(5, 1, 7),  -- Analytical
+(5, 2, 1),  -- Creative
+(5, 3, 2),  -- Social
+(5, 4, 6),  -- Organized
+(5, 5, 4);  -- Hands-on
 
--- Education
+-- 6. Education (3 + 4 + 7 + 4 + 2 = 20)
 INSERT INTO MajorTrait (major_id, trait_id, weight) VALUES
-(6, 1, 6),  -- Analytical
-(6, 2, 6),  -- Creative
-(6, 3, 9),  -- Social
-(6, 4, 7),  -- Organized
-(6, 5, 4);  -- Hands-on
+(6, 1, 3),  -- Analytical
+(6, 2, 4),  -- Creative
+(6, 3, 7),  -- Social
+(6, 4, 4),  -- Organized
+(6, 5, 2);  -- Hands-on
 
--- Economy
+-- 7. Economy (6 + 3 + 2 + 7 + 2 = 20)
 INSERT INTO MajorTrait (major_id, trait_id, weight) VALUES
-(7, 1, 9),  -- Analytical
-(7, 2, 6),  -- Creative
-(7, 3, 5),  -- Social
-(7, 4, 9),  -- Organized
-(7, 5, 3);  -- Hands-on
+(7, 1, 6),  -- Analytical
+(7, 2, 3),  -- Creative
+(7, 3, 2),  -- Social
+(7, 4, 7),  -- Organized
+(7, 5, 2);  -- Hands-on
+
 
